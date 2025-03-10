20843fdc85ab3777c7185e3b1a883d1185b40fac
mskk0803 <toyama2ura@gmail.com>
2025-03-10 11:05:58 +0900

    2025-03-10 postmodelの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_09_084631) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_10_020240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", primary_key: "uid", id: :string, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +34,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_09_084631) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
