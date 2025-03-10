bf44e5730e21f7317b8f9a44681278e30d85d0aa
mskk0803 <toyama2ura@gmail.com>
2025-03-10 11:12:30 +0900

    2025-03-10 postsのルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get "posts/index"
-  get "posts/new"
-  get "posts/create"
-  get "posts/destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -10,6 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  # 2025-03-10 postのルーティング定義
+  resources :posts, only: %i[index, new, create, destroy]
 
   # 2025-03-09 devise導入。各画面表示用のルーティング定義。
   devise_for :users, controllers: {
