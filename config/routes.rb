3bb000ac73e9c84f7d456c626ce470e9d6e0e7c1
mskk0803 <toyama2ura@gmail.com>
2025-03-10 11:02:22 +0900

    2025-03-10 postcontrollerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/new"
+  get "posts/create"
+  get "posts/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
