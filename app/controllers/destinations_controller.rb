34834b0afa7d6b889575dc3d918c83d125538932
mskk0803 <toyama2ura@gmail.com>
2025-03-15 13:09:33 +0900

    2025-03-12　Railsとの緯度経度のやり取りを実装

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,4 +1,17 @@
 class DestinationsController < ApplicationController
   def new
   end
+
+  def get_location
+    # 2025-03-15 destinationの位置情報取得処理
+    # 位置情報を取得するためのパラメータを取得
+    lat = params[:lat]
+    lon = params[:lon]
+
+    render json: { latitude: lat, longitude: lon }
+  end
+
+  # def get_stations
+
+  # end
 end
