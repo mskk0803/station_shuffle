31305f7704327028c651b0aa620cc1eb0b6ee9e2
mskk0803 <toyama2ura@gmail.com>
2025-03-16 15:35:27 +0900

    2025-03-16 現在地付近の駅を取得して表示

--- app/javascript/controllers/destinations_controller.js
+++ app/javascript/controllers/destinations_controller.js
@@ -60,17 +60,52 @@ export default class extends Controller {
     })
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log(data.stations)
+      console.log(!data.stations.length)
       if (this.hasListTarget) { 
-        // let stationElement = document.createElement("p");
-        // stationElement.textContent = `緯度: ${data.latitude}, 経度: ${data.longitude}, 半径: ${data.radius}`;
-        // this.listTarget.appendChild(stationElement);
-      } else {
-        console.error("listTarget が見つかりません");
+        // リストが空の場合
+        // 参考：https://www.freecodecamp.org/japanese/news/check-if-javascript-array-is-empty-or-not-with-length/
+        if (!data.stations.length){
+          this.listTarget.innerHTML = "";
+          const noStationsTextEle = document.createElement("p");
+          noStationsTextEle.textContent = "指定の検索範囲では駅が見つかりませんでした。"
+          this.listTarget.appendChild(noStationsTextEle);
+        }else{
+          this.updateStationList(data.stations);
+        }
       }
     })
     .catch((error) => {
       console.error('Error:', error);
     })
+  }
+
+  // 駅リストの更新関数
+  updateStationList(stations){
+    this.listTarget.innerHTML = "";
+    stations.forEach((stationName) => {
+      this.listTarget.appendChild(this.createCheckbox(stationName))
+    })
+
+  }
+
+  // チェックボックス生成
+  createCheckbox(stationName){
+    const divElement = document.createElement("div");
+    const checkBoxElement = document.createElement("input");
+    const labelElement = document.createElement("label");
+
+    checkBoxElement.type = "checkbox";
+    checkBoxElement.id = stationName;
+    checkBoxElement.name = "stations";
+    checkBoxElement.value = stationName;
+
+    labelElement.htmlFor = stationName;
+    labelElement.textContent = stationName;
+
+    divElement.appendChild(checkBoxElement);
+    divElement.appendChild(labelElement);
+
+    return divElement;
   }
 }
