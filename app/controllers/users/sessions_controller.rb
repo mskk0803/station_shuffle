44f5a873f3508771fe883466ad23286f93a03593
mskk0803 <toyama2ura@gmail.com>
2025-03-21 09:47:31 +0900

    2025-03-21 ログインの制御を変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,6 +11,7 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     super
+    posts_path
   end
 
   # DELETE /resource/sign_out
@@ -19,10 +20,14 @@ class Users::SessionsController < Devise::SessionsController
     posts_path
   end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
 end
