4aa246cd23022abc235526aaa988f50427b23d09
mskk0803 <toyama2ura@gmail.com>
2025-04-11 06:20:12 +0900

    mypagecontroller追加

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,9 @@ Rails.application.routes.draw do
   # 2025-03-19 likeのルーティング定義
   resources :likes, only: %i[create destroy]
 
+  # 2025-04-11 mypageのルーティング定義
+  resources :mypages, only: %i[show]
+
   # 2025-03-09 devise導入。各画面表示用のルーティング定義。
   devise_for :users, controllers: {
     registrations: "users/registrations",
