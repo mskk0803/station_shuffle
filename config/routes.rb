3f7c9c7c81e386151bec3624ffab6cb7bff8b0ad
mskk0803 <toyama2ura@gmail.com>
2025-04-20 14:05:14 +0900

    2025-04-20 フォロー時のIndexの表示を制御

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
 
   # 2025-03-10 postのルーティング定義
   resources :posts, only: %i[index new create destroy] do
-    # collection do
-    #   get :likes
-    # end
+    collection do
+      get :following_index
+      get :all_index
+    end
   end
 
   # 2025-03-15 destinationのルーティング定義
