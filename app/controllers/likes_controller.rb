4e4a782509cb7236b9d86edd76afb5df9f1e522d
mskk0803 <toyama2ura@gmail.com>
2025-04-20 11:40:59 +0900

    2025-04-20 Likeボタンの挙動を変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,14 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     current_user.like(post)
-    redirect_to posts_path, success: "Like Post"
+    # もといた画面に遷移する
+    # 参考URL:https://qiita.com/sew_sou19/items/21974ec512c0a9b329aa
+    redirect_to request.referer, success: "Like Post"
   end
 
   def destroy
     post = current_user.likes.find(params[:id]).post
     current_user.unlike(post)
-    redirect_to posts_path, success: "Like Delete", status: :see_other
+    redirect_to request.referer, success: "Like Delete", status: :see_other
   end
 end
