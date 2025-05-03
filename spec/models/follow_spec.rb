c7ad11023a6e27b1eb9f22be7e3c3a2c0beca5df
mskk0803 <toyama2ura@gmail.com>
2025-05-03 15:41:12 +0900

    2025-05-03 Followモデルスペックの修正

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  describe "バリデーション" do
+  describe "バリデーションが有効な場合" do
     let(:user1){ create(:user) }
     let(:user2){ create(:user) }
+
     it "有効な場合は保存できる" do
       follow = build(:follow, follower: user1, followed: user2)
       expect(follow).to be_valid
@@ -14,7 +15,11 @@ RSpec.describe Follow, type: :model do
       follow2 = create(:follow, follower: user2, followed: user1)
       expect(follow2).to be_valid
     end
-
+  end
+  describe "無効な場合" do
+    let(:user1){ create(:user) }
+    let(:user2){ create(:user) }
+    
     it "同じユーザーを2回フォローできない（組み合わせがユニークでない）" do
       create(:follow, follower: user1, followed: user2)
       duplicate = build(:follow, follower: user1, followed: user2)
