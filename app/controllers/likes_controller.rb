5bc9e5476774d5b311ee1e230d68ca185b813487
mskk0803 <toyama2ura@gmail.com>
2025-05-09 08:56:03 +0900

    2025-05-09 リダイレクト時のメッセージを変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,12 +12,12 @@ class LikesController < ApplicationController
     post.user.create_notification(like)
     # もといた画面に遷移する
     # 参考URL:https://qiita.com/sew_sou19/items/21974ec512c0a9b329aa
-    redirect_to request.referer || root_path, success: "Like Post"
+    redirect_to request.referer || root_path, success: "いいねしました。"
   end
 
   def destroy
     post = current_user.likes.find(params[:id]).post
     current_user.unlike(post)
-    redirect_to request.referer || root_path, success: "Like Delete", status: :see_other
+    redirect_to request.referer || root_path, success: "いいねを取り消しました。", status: :see_other
   end
 end
