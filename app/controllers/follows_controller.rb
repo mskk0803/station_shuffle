b346bcedc49e394da8d83f7dbfd4c9fb55a598cd
mskk0803 <toyama2ura@gmail.com>
2025-05-09 12:57:47 +0900

    2025-05-09 ボタンをturboに変更

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,27 +1,36 @@
 class FollowsController < ApplicationController
   def create
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
 
-    if user.is_private?
+    if @user.is_private?
       # 非公開ユーザーにフォローリクエストを送信
-      current_user.send_request(user)
-      follow_request = current_user.follow_requests.find_by(requestee_id: user.id)
+      current_user.send_request(@user)
+      follow_request = current_user.follow_requests.find_by(requestee_id: @user.id)
       # 通知を作成
-      user.create_notification(follow_request)
-      redirect_to profile_posts_path(user.id), notice: "フォローリクエストを送りました。。"
+      @user.create_notification(follow_request)
+      redirect_to profile_posts_path(@user.id), notice: "フォローリクエストを送りました。。"
     else
-      current_user.follow(user)
-      follow = current_user.follows.find_by(followed_user_id: user.id)
+      current_user.follow(@user)
+      follow = current_user.follows.find_by(followed_user_id: @user.id)
 
       # 通知を作成
-      user.create_notification(follow)
-      redirect_to profile_posts_path(user.id), notice: "フォローしました。"
+      @user.create_notification(follow)
+
+      # turboで表示
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @user }
+      end
     end
   end
 
   def destroy
-    user = current_user.following.find_by(id: params[:id])
-    current_user.unfollow(user)
-    redirect_to profile_posts_path(user.id), notice: "フォローを外しました。", status: :see_other
+    @user = current_user.following.find_by(id: params[:id])
+    current_user.unfollow(@user)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @user }
+    end
   end
 end
